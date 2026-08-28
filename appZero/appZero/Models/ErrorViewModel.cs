b71c403a81f39a7aca7c43d2bc927d87ea2c316f
Layssa Barros <layssa.silvas09@gmail.com>
2026-08-28 09:42:17 -0300

    criar funcionario controller

---
+++ appZero/appZero/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace appZero.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ appZero/appZero/Models/dbContextTask.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace appZero.Models
+{
+    public class dbContextTask
+    {
+        public class DbContextTask : DbContext
+        {
+
+            public DbContextTask()
+            {
+            }
+
+            public DbContextTask(DbContextOptions<DbContextTask> options)
+                : base(options)
+            {
+            }
+
+        }
+    }
+}
